e8e5832da6bea2c317c64451defcbc571ee915be
Alejandro Melendez <alejandromelendez99@outlook.es>
2022-04-02 14:22:59 -0600

    Pantallas del dashboard

---
+++ admin/auth/login.php
@@ -0,0 +1,10 @@
+<?php 
+    //Vars
+    $title='Login'; 
+?>
+
+<?php require("../resources/templates/header-template.php"); ?>
+
+    <?php require("../resources/components/login-component.php"); ?>
+
+<?php require("../resources/templates/footer-template.php");?>
