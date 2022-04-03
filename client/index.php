17d7e8faed88c78e631891a08ac907656069e996
Alejandro Melendez <alejandromelendez99@outlook.es>
2022-04-03 01:53:28 -0600

    Agregando pantallas de la tienda

--- client/index.php
+++ client/index.php
@@ -1,1 +1,10 @@
-Hola mundo
+<?php $title='Inicio'; ?>
+<?php $index='index.php'; ?>
+<?php $about='pages/aboutUs.php'; ?>
+<?php $css='resources/css/store.css'; ?>
+
+<?php require("resources/templates/header-template.php"); ?>
+    
+    <?php require("resources/components/store-component.php"); ?>
+
+<?php require("resources/templates/footer-template.php"); ?>
